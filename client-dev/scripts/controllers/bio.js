3f1150d0220e530ca5e220ea8dea146f06d2951d
Ivar Prudnikov <ivar.prudnikov@gmail.com>
2015-11-17 17:14:09 +0000

    bugfix: bio page animation fixes

--- client-dev/scripts/controllers/bio.js
+++ client-dev/scripts/controllers/bio.js
@@ -48,65 +48,69 @@ angular.module('tApp')
 		//////////////////////////
 
 		$scope.codeDataBackEnd = [
-			{
-				lang:'node.js',
-				text:'Node.js is a platform built on Chrome\'s JavaScript runtime for easily building fast, scalable network applications.'
-			},
-			{
-				lang:'groovy',
-				text:'Groovy is an object-oriented programming language for the Java platform. It is a dynamic language with features similar to those of Python, Ruby, Perl, and Smalltalk.'
-			},
-			{
-				lang:'grails',
-				text:'Grails is an open source web application framework that uses the Groovy programming language.'
-			},
-			{
-				lang:'java',
-				text:'Java is a general-purpose, concurrent, class-based, object-oriented computer programming language that is specifically designed to have as few implementation dependencies as possible.'
-			},
-			{
-				lang:'mysql',
-				text:'the world\'s most widely used open-source relational database management system'
-			},
-			{
-				lang:'hibernate',
-				text:'Object-relational mapping (ORM) library for the Java language, providing a framework for mapping an object-oriented domain model to a traditional relational database.'
-			},
+      {
+        lang:'cassandra',
+        text:'The Apache Cassandra database is the right choice when you need scalability and high availability without compromising performance.'
+      },
+      {
+        lang:'grails',
+        text:'Grails is an open source web application framework that uses the Groovy programming language.'
+      },
+      {
+        lang:'groovy',
+        text:'Groovy is an object-oriented programming language for the Java platform. It is a dynamic language with features similar to those of Python, Ruby, Perl, and Smalltalk.'
+      },
+      {
+        lang:'hibernate',
+        text:'Object-relational mapping (ORM) library for the Java language, providing a framework for mapping an object-oriented domain model to a traditional relational database.'
+      },
+      {
+        lang:'java',
+        text:'Java is a general-purpose, concurrent, class-based, object-oriented computer programming language that is specifically designed to have as few implementation dependencies as possible.'
+      },
 			{
 				lang:'mongodb',
 				text:'MongoDB is a cross-platform document-oriented database system.'
-			}
+			},
+      {
+        lang:'mysql',
+        text:'the world\'s most widely used open-source relational database management system'
+      },
+      {
+        lang:'node.js',
+        text:'Node.js is a platform built on Chrome\'s JavaScript runtime for easily building fast, scalable network applications.'
+      }
 		];
 
 		$scope.codeDataFrontEnd = [
-			{
-				lang:'html',
-				text:'HyperText Markup Language (HTML) is the main markup language for creating web pages and other information that can be displayed in a web browser.'
-			},
-			{
-				lang:'jade',
-				text:'Jade is a high performance template engine heavily influenced by Haml and implemented with JavaScript for node.'
-			},
-			{
-				lang:'css',
-				text:'Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation semantics (the look and formatting) of a document written in a markup language.'
-			},
-			{
-				lang:'less',
-				text:'The dynamic stylesheet language. LESS extends CSS with dynamic behavior such as variables, mixins, operations and functions.'
-			},
-			{
-				lang:'javascript',
-				text:'JavaScript is a prototype-based scripting language with dynamic typing and has first-class functions.'
-			},
+      {
+        lang:'angular',
+        text:'AngularJS is what HTML would have been, had it been designed for building web-apps.'
+      },
+      {
+        lang:'android',
+        text:'Android is the customizable, easy to use operating system that powers more than a billion devices across the globe.'
+      },
 			{
 				lang:'backbone',
 				text:'Backbone supplies structure to JavaScript-heavy applications.'
 			},
-			{
-				lang:'angular',
-				text:'AngularJS is what HTML would have been, had it been designed for building web-apps.'
-			}
+      {
+        lang:'css/less/sass',
+        text:'Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation semantics (the look and formatting) of a document written in a markup language.'
+      },
+      {
+        lang:'html/jade',
+        text:'HyperText Markup Language (HTML) is the main markup language for creating web pages and other information that can be displayed in a web browser.'
+      },
+      {
+        lang:'javascript',
+        text:'JavaScript is a prototype-based scripting language with dynamic typing and has first-class functions.'
+      },
+      {
+        lang:'sencha/cordova/phonegap',
+        text:'JavaScript frameworks for building cross-platform mobile web applications.'
+      }
 		];
 
 		// EXPERTISE
